5c5e6ac2c09583e69cd29f18823a15c533240127
ChineDmitri <76706642+ChineDmitri@users.noreply.github.com>
2021-07-04 14:34:24 +0200

    Utulisation .escape et .escapeId et amelioration du structure avec le promise

--- mysql/db.js
+++ mysql/db.js
@@ -9,28 +9,50 @@ const pool = mysql.createPool({
     port: '3306'
 })
 
-let elevagedb = {};
+// let elevagedb = {};
 
-elevagedb.all = () => {
+// elevagedb.all = () => {
+//     return new Promise((resolve, reject) => {
+//         pool.query('SELECT * FROM Animal', (err, results) => {
+//             if (err) {
+//                 return reject(err);
+//             } 
+//             return resolve(results);
+//         })
+//     })
+// }
+
+// COMME CA 
+// exports.getOne = (espece) => {
+//         return new Promise((resolve, reject) => {
+//             pool.query('SELECT * FROM Animal WHERE espece = ?', [espece], (err, results) => {
+//                 if (err || results.length === 0) {
+//                     return reject(results);
+//                 } 
+//                 return resolve(results);
+//             })
+//         })   
+// }
+
+// CAS DE NULL
+exports.getOne = (espece) => {
     return new Promise((resolve, reject) => {
-        pool.query('SELECT * FROM Animal', (err, results) => {
-            if (err) {
-                return reject(err);
+        pool.query("SELECT * FROM Animal WHERE espece = ? && nom IS NOT NULL", [espece], (err, results) => {
+            if (err || results.length === 0) {
+                return reject(results);
             } 
             return resolve(results);
         })
-    })
+    })   
 }
 
-elevagedb.one = (espece) => {
+exports.getOneSortByDate = (espece, sort) => {
     return new Promise((resolve, reject) => {
-        pool.query('SELECT * FROM Animal WHERE espece = ?', [espece], (err, results) => {
-            if (err) {
-                return reject(err);
+        pool.query(`SELECT * FROM Animal WHERE espece = ${pool.escape(espece)} ORDER BY ${pool.escapeId(sort)}`, (err, results) => {
+            if (err || results.length === 0) {
+                return reject(results);
             } 
             return resolve(results);
         })
-    })
+    })   
 }
-
-module.exports = elevagedb;
--- routes/animal.js
+++ routes/animal.js
@@ -1,28 +1,39 @@
 const express = require('express');
 const elevagedb = require('../mysql/db');
+// import { getOne } from '../mysql/db.js';
+// const getOne = require('../mysql/db');
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-    // res.status(200).json({ test: 'test OK'});
-    try {
-        let results = await elevagedb.all();
-        res.json(results);
-    } catch(error) {
-        console.log(error);
-        res.status(501).json({ error });
-    }
+// router.get('/', async (req, res, next) => {
+//     // res.status(200).json({ test: 'test OK'});
+//     try {
+//         let results = await elevagedb.all();
+//         res.json(results);
+//     } catch(error) {
+//         console.log(error);
+//         res.status(500).json({ error });
+//     }
+// });
+
+router.get('/:espece', (req, res, next) => {
+    elevagedb.getOne(req.params.espece)
+    .then((animal) => {
+        res.status(200).json(animal);    
+    })
+    .catch(() => {
+        res.status(404).json({ error: "message" });
+    }) 
 });
 
-router.get('/:espece', async (req, res, next) => {
-    // res.status(200).json({ test: 'test OK'});
-    try {
-        let results = await elevagedb.one(req.params.espece);
-        res.json(results);
-    } catch(error) {
-        console.log(error);
-        res.status(501).json({ error });
-    }
+router.get('/:espece/:sort', (req, res, next) => {
+    elevagedb.getOneSortByDate(req.params.espece, req.params.sort)
+    .then((animal) => {
+        res.status(200).json(animal);    
+    })
+    .catch(() => {
+        res.status(404).json({ error: "message" });
+    }) 
 });
 
 module.exports = router;
